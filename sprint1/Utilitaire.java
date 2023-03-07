da1e8a491845f1d88764f5b5cd16a4529889beed
ains1829 <andyrakotonavalona0@gmail.com>
2023-03-07 07:03:34 +0100

    ok

---
+++ sprint1/Utilitaire.java
@@ -0,0 +1,15 @@
+package utilitaire;
+public class Utilitaire {
+    
+    public Utilitaire() {
+    }
+    public void getAllProcess(){
+        System.out.println("all proces");
+    }
+    public void getInsert(){
+        System.out.println("all insert");
+    }
+    public void getDonnee(){
+        System.out.println("all Donne");
+    }
+}
